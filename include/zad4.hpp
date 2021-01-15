92144fef503f032b53d55c80a7615d3061a0db35
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 08:45:04 +0000

    zad4v2

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -9,9 +9,7 @@ std::vector<Artysta> stworzZespol(int number, const std::string& name)
     std::vector<Artysta> wk;
     if (name.size() != number)
         throw std::logic_error("blad");
-    
-    for(int i = 1; i < liczba+1; i++)
+    for(int i = 1; i < number+1; i++)
         wk.push_back(Artysta{name, static_cast<unsigned>(i)});
-    
     return wk;
 }
