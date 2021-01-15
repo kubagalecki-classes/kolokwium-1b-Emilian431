d93983a2f93218a792fc3148cc5728ae0f52ac2e
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 07:36:51 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,23 @@
 #include <string>
 
 // tutaj klasa Artysta
+
+class Artysta
+{
+  public:
+  Artysta(): pseudonim{"Anonim"}, liczbasluchaczy{0} {}
+  Artysta(const std::string& ps, unsigned int ls) : pseudonim{ps}, liczbasluchaczy{ls} {}
+  void graj(std::ostream&) const {
+    std::cout << pseudonim << liczbasluchaczy << endl;
+  }
+  const std::string& getPseudonim() const{
+      return pseudonim;
+  }
+  unsigned getSluchacze() const{
+      return liczbasluchaczy;
+  }
+  private:
+  std::string pseudonim;
+  unsigned int liczbasluchaczy;
+
+};
