a79a86dbb2b207185d30b5edf4fde69801a0d8d1
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 08:40:54 +0000

    zad3

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,13 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template<typename T>
+class ArtystaPlusInstrument
+{
+public:
+    ArtystaPlusInstrument(const Artysta& a_, const T& i_) : a{a_}, i{i_} {}
+    unsigned koncert() {return i.liczbaUczestnikow(a);}
+private:
+    Artysta a;
+    T i;
+};
