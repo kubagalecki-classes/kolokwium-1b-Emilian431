2b390bf06f92f78b3f5d9f64e6d439b48f5af84a
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 08:47:24 +0000

    zad5

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -5,3 +5,8 @@
 #include <vector>
 
 // tutaj algorytm liczPopularnych(T begin, T end)
+template<typename T>
+unsigned liczPopularnych(T e1, T e2)
+{
+    return std::count_if(e1, e2, [](const Artysta& o){return o.getSluchacze() > 1000;});
+}
