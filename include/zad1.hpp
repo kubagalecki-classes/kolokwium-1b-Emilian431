be6ca1fda2c7a77f712204d71f9704c423069c28
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 07:41:54 +0000

    zad1v1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -13,8 +13,8 @@ class Artysta
   public:
   Artysta(): pseudonim{"Anonim"}, liczbasluchaczy{0} {}
   Artysta(const std::string& ps, unsigned int ls) : pseudonim{ps}, liczbasluchaczy{ls} {}
-  void graj(std::ostream&) const {
-    std::cout << pseudonim << liczbasluchaczy << endl;
+  void graj(std::ostream& art) const {
+    art << pseudonim << ": " << liczbasluchaczy;
   }
   const std::string& getPseudonim() const{
       return pseudonim;
