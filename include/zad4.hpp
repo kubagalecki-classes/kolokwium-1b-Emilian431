33f05d0433d9f9fe5a18e6fee49da6e285f14a6c
Emilian431 <emilian.horoszko@gmail.com>
2021-01-15 08:44:02 +0000

    zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,14 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+std::vector<Artysta> stworzZespol(int number, const std::string& name)
+{
+    std::vector<Artysta> wk;
+    if (name.size() != number)
+        throw std::logic_error("blad");
+    
+    for(int i = 1; i < liczba+1; i++)
+        wk.push_back(Artysta{name, static_cast<unsigned>(i)});
+    
+    return wk;
+}
